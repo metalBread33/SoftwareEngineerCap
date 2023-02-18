2c3ac6f3fce48b5a50e32a02e56cf7191fe4e060
Kevin Foughty <97193605+metalBread33@users.noreply.github.com>
2023-02-18 16:02:08 -0500

    Update default-2021.dwlt
    Began working on pause menu

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public bool paused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused) resume();
+            else pause();
+        }        
+    }
+
+    public void pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+    }
+
+    public void resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+    }
+}
