efecbd701aa096b3d620cb292548c4af688ff369
Kevin Foughty <97193605+metalBread33@users.noreply.github.com>
2023-02-18 17:27:35 -0500

    Pause game menu and script created

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseGame : MonoBehaviour
 {
     public GameObject pauseMenu;
-    public bool paused;
+    public bool paused = false;
     // Start is called before the first frame update
     void Start()
     {
