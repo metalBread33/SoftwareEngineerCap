f151951cf421b61ed220e7606b7144f011f73fab
Kevin Foughty <97193605+metalBread33@users.noreply.github.com>
2023-02-24 13:16:09 -0500

    Worked on audio manager

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -1,19 +1,25 @@
 using UnityEngine.Audio;
 using UnityEngine;
-
+using System;
 public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        foreach(Sound sound in sounds) 
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+        }  
     }
 
-    // Update is called once per frame
-    void Update()
+    public void play(string name)
     {
-        
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+       // s.source.play();
     }
 }
--- Assets/Sound.cs
+++ Assets/Sound.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 [System.Serializable]
 public class Sound
 {
+    public string name;
     public AudioClip clip;
 
-    public float volume, pitch;
+    [Range (0f, 1f)] 
+    public float volume;
+    [Range (.1f, 3f)] 
+    public float pitch;
+
+    [HideInInspector]
+    public AudioSource source;
 }
